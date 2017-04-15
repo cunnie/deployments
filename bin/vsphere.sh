f2282525dbe420cefccdfc283c23db82bf640756
Brian Cunnie <brian.cunnie@gmail.com>
2017-04-14 17:59:01 -0700

    BOSH vSphere Director is multi-homed

--- bin/vsphere.sh
+++ bin/vsphere.sh
@@ -14,8 +14,8 @@ DEPLOYMENTS_DIR=~/workspace/deployments/
 cat > $DEPLOYMENTS_DIR/bosh-vsphere.yml <<EOF
 # DON'T EDIT; THIS FILE IS AUTO-GENERATED
 #
-# bosh2 create-env bosh-vsphere.yml -l <(lpass show --note deployments) -l vsphere-creds.yml -l <(curl -L https://raw.githubusercontent.com/cunnie/sslip.io/master/conf/sslip.io%2Bnono.io.yml)
-# bosh2 -e bosh-vsphere.nono.io --ca-cert <(bosh2 int vsphere-creds.yml --path /director_ssl/ca) alias-env vsphere
+# bosh2 create-env bosh-vsphere.yml -l <(lpass show --note deployments-vsphere) -l vsphere-creds.yml
+# bosh2 -e 10.85.46.6 --ca-cert <(bosh2 int vsphere-creds.yml --path /director_ssl/ca) alias-env nsx-t
 #
 EOF
 
@@ -24,32 +24,18 @@ bosh2 interpolate ~/workspace/bosh-deployment/bosh.yml \
   -o ~/workspace/bosh-deployment/jumpbox-user.yml \
   -o etc/vsphere.yml \
   --vars-store $DEPLOYMENTS_DIR/vsphere-creds.yml \
+  -v vcenter_user=root \
+  -v vcenter_dc=private \
+  -v vcenter_cluster=private \
+  -v vcenter_resource_pool=mouse \
+  -v vcenter_ds=vnx5600-pizza-2 \
+  -v vcenter_disks=delete-me \
+  -v vcenter_vms=delete-me \
+  -v vcenter_templates=delete-me \
+  -v director_name=nsx-t \
+  -v network_name=mouse \
+  -v internal_ip=10.85.46.6 \
+  -v internal_gw=10.85.46.1 \
+  -v internal_cidr=10.85.46.0/24 \
+  -v director_name=nsx-t \
   >> $DEPLOYMENTS_DIR/bosh-vsphere.yml
-
-exit
-
-  -o etc/aws.yml \
-  -o etc/nginx.yml \
-  -o etc/ntp.yml \
-  -o etc/pdns.yml \
-  -v access_key_id="((aws_access_key_id))" \
-  -v secret_access_key="((aws_secret_access_key))" \
-  -v region=us-east-1 \
-  -v az=us-east-1a \
-  -v default_key_name=aws_nono \
-  -v default_security_groups=[bosh] \
-  -v subnet_id=subnet-1c90ef6b \
-  -v director_name=bosh-aws \
-  -v internal_cidr=10.0.0.0/24 \
-  -v internal_gw=10.0.0.1 \
-  -v internal_ip=10.0.0.6 \
-  -v external_ip=52.0.56.137 \
-  -v admin_password="((admin_password))" \
-  -v blobstore_agent_password="((blobstore_agent_password))" \
-  -v blobstore_director_password="((blobstore_director_password))" \
-  -v hm_password="((hm_password))" \
-  -v mbus_bootstrap_password="((mbus_bootstrap_password))" \
-  -v nats_password="((nats_password))" \
-  -v postgres_password="((postgres_password))" \
-  -v registry_password="((registry_password))" \
-  -v private_key=$HOME/.ssh/aws_nono.pem \
