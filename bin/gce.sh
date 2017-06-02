f1a9b8cdc389d021750b3620bb9a30b60de7b175
Brian Cunnie <brian.cunnie@gmail.com>
2017-06-02 10:35:32 +0800

    WIP: BOSH GCE is deployed via bosh-deployment
    - and customizations

--- bin/gce.sh
+++ bin/gce.sh
@@ -11,8 +11,6 @@
 #
 DEPLOYMENTS_DIR=~/workspace/deployments/
 
-echo "not ready yet: need to make this BOSH specific"
-
 cat > $DEPLOYMENTS_DIR/bosh-gce.yml <<EOF
 # DON'T EDIT; THIS FILE IS AUTO-GENERATED
 #
@@ -21,34 +19,31 @@ cat > $DEPLOYMENTS_DIR/bosh-gce.yml <<EOF
 #
 EOF
 
-bosh interpolate ~/workspace/bosh-deployment/bosh.yml \
-  -o ~/workspace/bosh-deployment/gce/cpi.yml \
-  -o ~/workspace/bosh-deployment/external-ip-with-registry-not-recommended.yml \
+bosh2 interpolate ~/workspace/bosh-deployment/bosh.yml \
+  -o ~/workspace/bosh-deployment/gcp/cpi.yml \
+  -o ~/workspace/bosh-deployment/external-ip-not-recommended.yml \
   -o ~/workspace/bosh-deployment/jumpbox-user.yml \
   -o etc/gce.yml \
-  -o etc/nginx.yml \
-  -o etc/ntp.yml \
-  -o etc/pdns.yml \
-  --vars-store $DEPLOYMENTS_DIR/gce-creds.yml \
-  -v access_key_id="((gce_access_key_id))" \
-  -v secret_access_key="((gce_secret_access_key))" \
-  -v region=us-east-1 \
-  -v az=us-east-1a \
-  -v default_key_name=gce_nono \
-  -v default_security_groups=[bosh] \
-  -v subnet_id=subnet-1c90ef6b \
-  -v director_name=bosh-gce \
-  -v internal_cidr=10.0.0.0/24 \
-  -v internal_gw=10.0.0.1 \
-  -v internal_ip=10.0.0.6 \
-  -v external_ip=52.0.56.137 \
-  -v admin_password="((admin_password))" \
-  -v blobstore_agent_password="((blobstore_agent_password))" \
-  -v blobstore_director_password="((blobstore_director_password))" \
-  -v hm_password="((hm_password))" \
-  -v mbus_bootstrap_password="((mbus_bootstrap_password))" \
-  -v nats_password="((nats_password))" \
-  -v postgres_password="((postgres_password))" \
-  -v registry_password="((registry_password))" \
-  -v private_key=$HOME/.ssh/gce_nono.pem \
+  --vars-store $DEPLOYMENTS_DIR/gce-creds.yml
   >> $DEPLOYMENTS_DIR/bosh-gce.yml
+#   -v access_key_id="((gce_access_key_id))" \
+#   -v secret_access_key="((gce_secret_access_key))" \
+#   -v region=us-east-1 \
+#   -v az=us-east-1a \
+#   -v default_key_name=gce_nono \
+#   -v default_security_groups=[bosh] \
+#   -v subnet_id=subnet-1c90ef6b \
+#   -v director_name=bosh-gce \
+#   -v internal_cidr=10.0.0.0/24 \
+#   -v internal_gw=10.0.0.1 \
+#   -v internal_ip=10.0.0.6 \
+#   -v external_ip=52.0.56.137 \
+#   -v admin_password="((admin_password))" \
+#   -v blobstore_agent_password="((blobstore_agent_password))" \
+#   -v blobstore_director_password="((blobstore_director_password))" \
+#   -v hm_password="((hm_password))" \
+#   -v mbus_bootstrap_password="((mbus_bootstrap_password))" \
+#   -v nats_password="((nats_password))" \
+#   -v postgres_password="((postgres_password))" \
+#   -v registry_password="((registry_password))" \
+#   -v private_key=$HOME/.ssh/gce_nono.pem \
