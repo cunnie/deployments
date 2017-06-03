4df8d466e3782972558adf14c88f547bf5c89082
Brian Cunnie <brian.cunnie@gmail.com>
2017-06-02 18:36:28 -0700

    Dynamic networking requires PowerDNS
    fixes:
    ```
    failed to migrate database: dial tcp: lookup 8a2baf66-3fb9-4337-a7a6-f3542efc3359.concourse.dynamic.concourse.bosh on 10.128.0.2:53: read udp 10.128.0.3:39597->10.128.0.2:53: read: connection refused
    ```

--- bin/gce.sh
+++ bin/gce.sh
@@ -14,17 +14,19 @@ DEPLOYMENTS_DIR=~/workspace/deployments/
 cat > $DEPLOYMENTS_DIR/bosh-gce.yml <<EOF
 # DON'T EDIT; THIS FILE IS AUTO-GENERATED
 #
-# bosh2 create-env bosh-gce.yml -l <(lpass show --note deployments) -l gce-creds.yml -l <(curl -L https://raw.githubusercontent.com/cunnie/sslip.io/master/conf/sslip.io%2Bnono.io.yml)
-# bosh2 -e bosh-gce.nono.io --ca-cert <(bosh int gce-creds.yml --path /director_ssl/ca) alias-env gce
+# bosh2 create-env bosh-gce.yml -l <(lpass show --note deployments)
+# bosh2 -e bosh-gce.nono.io alias-env gce
 #
 EOF
 
 bosh2 interpolate ~/workspace/bosh-deployment/bosh.yml \
+  -o ~/workspace/bosh-deployment/powerdns.yml \
   -o ~/workspace/bosh-deployment/gcp/cpi.yml \
   -o ~/workspace/bosh-deployment/external-ip-not-recommended.yml \
   -o ~/workspace/bosh-deployment/jumpbox-user.yml \
   -o etc/gce.yml \
   --vars-store $DEPLOYMENTS_DIR/gce-creds.yml \
+  -v dns_recursor_ip="169.254.169.254" \
   -v internal_gw="10.128.0.1" \
   -v internal_cidr="10.128.0.0/20" \
   -v internal_ip="10.128.0.2" \
