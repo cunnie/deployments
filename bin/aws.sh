980ae18de7d82b8e1c66b0d6b55eda05e2c7950c
Brian Cunnie <brian.cunnie@gmail.com>
2017-09-01 21:15:09 -0700

    Successful GCE bump & AWS bump

--- bin/aws.sh
+++ bin/aws.sh
@@ -27,9 +27,7 @@ bosh interpolate ~/workspace/bosh-deployment/bosh.yml \
   -o etc/nginx.yml \
   -o etc/ntp.yml \
   -o etc/pdns.yml \
-  --vars-store $DEPLOYMENTS_DIR/aws-creds.yml \
-  -v access_key_id="((aws_access_key_id))" \
-  -v secret_access_key="((aws_secret_access_key))" \
+  --var-file nono_io_crt=etc/nono.io.crt \
   -v region=us-east-1 \
   -v az=us-east-1a \
   -v default_key_name=aws_nono \
@@ -40,13 +38,5 @@ bosh interpolate ~/workspace/bosh-deployment/bosh.yml \
   -v internal_gw=10.0.0.1 \
   -v internal_ip=10.0.0.6 \
   -v external_ip=52.0.56.137 \
-  -v admin_password="((admin_password))" \
-  -v blobstore_agent_password="((blobstore_agent_password))" \
-  -v blobstore_director_password="((blobstore_director_password))" \
-  -v hm_password="((hm_password))" \
-  -v mbus_bootstrap_password="((mbus_bootstrap_password))" \
-  -v nats_password="((nats_password))" \
-  -v postgres_password="((postgres_password))" \
-  -v registry_password="((registry_password))" \
   -v private_key=$HOME/.ssh/aws_nono.pem \
   >> $DEPLOYMENTS_DIR/bosh-aws.yml
