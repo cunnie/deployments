94cba51a14e19c75a484ad7aab3b89bdd1c9343f
Brian Cunnie <brian.cunnie@gmail.com>
2017-09-27 14:20:28 -0700

    BOSH vSphere: bump, Resource Pool, script updates
    - BOSH is now placed in the `cunnie` resource pool to distinguish
      it from Dmitriy's IPv6 BOSH
    - BOSH's datastore is updated to SSD-1 ("SSD" no longer exists)

--- bin/vsphere.sh
+++ bin/vsphere.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #
 # usage:
 #   bin/vsphere.sh
@@ -6,27 +7,28 @@
 #
 # --var-errs: don't use; it flags the variables I'll interpolate the _next_ stage
 #
-DEPLOYMENTS_DIR=$PWD
+DEPLOYMENTS_DIR="$( cd "${BASH_SOURCE[0]%/*}" && pwd )/.."
 
 cat > $DEPLOYMENTS_DIR/bosh-vsphere.yml <<EOF
 # DON'T EDIT; THIS FILE IS AUTO-GENERATED
 #
 # bosh create-env bosh-vsphere.yml -l <(lpass show --note deployments.yml)
-#
-# bosh -e bosh-vsphere.nono.io vsphere
+# bosh -e bosh-vsphere.nono.io alias-env vsphere
 #
 EOF
 
-bosh interpolate ~/workspace/bosh-deployment/bosh.yml \
-  -o ~/workspace/bosh-deployment/vsphere/cpi.yml \
-  -o ~/workspace/bosh-deployment/jumpbox-user.yml \
+bosh interpolate $DEPLOYMENTS_DIR/../bosh-deployment/bosh.yml \
+  -o $DEPLOYMENTS_DIR/../bosh-deployment/vsphere/cpi.yml \
+  -o $DEPLOYMENTS_DIR/../bosh-deployment/vsphere/resource-pool.yml \
+  -o $DEPLOYMENTS_DIR/../bosh-deployment/jumpbox-user.yml \
   -o etc/vsphere.yml \
   --var-file nono_io_crt=etc/nono.io.crt \
   -v vcenter_ip=vcenter.nono.io \
   -v vcenter_user=administrator@vsphere.local \
   -v vcenter_dc=dc \
   -v vcenter_cluster=cl \
-  -v vcenter_ds=SSD \
+  -v vcenter_rp=cunnie \
+  -v vcenter_ds=SSD-1 \
   -v vcenter_disks=bosh-disks \
   -v vcenter_vms=bosh-vms \
   -v vcenter_templates=bosh-templates \
