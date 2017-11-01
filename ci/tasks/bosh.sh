7a608376910616132f53e6f40c03bec8fe0ed2ca
Brian Cunnie <brian.cunnie@gmail.com>
2017-10-31 21:15:06 -0700

    :beetle: Push BOSH `-state.json` on failed deploys
    Previously the state files were only pushed for successful deploys.
    This was a problem when a BOSH deployment failed after the new VM was
    created: the record of the new VM was never recorded in the manifest,
    and subsequent deploys would also fail.
    
    There were two aspects to the fix:
    - we used an `if` before the `bosh create-env`, which doesn't trigger an
    abort when `set -e` is in effect. This allows us to _commit_ the state
    file of the broken deploy.
    - we used an `ensure` block to _push_ the commit was pushed
    regardless of the task's success.
    
    fixes: <https://ci.nono.io/teams/main/pipelines/BOSH/jobs/bosh-azure.nono.io/builds/42>

--- ci/tasks/bosh.sh
+++ ci/tasks/bosh.sh
@@ -3,9 +3,14 @@ set -e
 
 pushd cunnie-deployments
 bin/$IAAS.sh
-bosh create-env bosh-$IAAS.yml \
+
+if bosh create-env bosh-$IAAS.yml \
   -l <(echo "$DEPLOYMENTS_YML") \
-  -l <(curl https://raw.githubusercontent.com/cunnie/sslip.io/master/conf/sslip.io%2Bnono.io.yml)
+  -l <(curl https://raw.githubusercontent.com/cunnie/sslip.io/master/conf/sslip.io%2Bnono.io.yml); then
+  GIT_COMMIT_MESSAGE="CI PASS: $IAAS BOSH deploy :airplane:"
+else
+  GIT_COMMIT_MESSAGE="CI FAIL: $IAAS BOSH deploy :airplane:"
+fi
 
 if ! git diff --quiet HEAD --; then
   git config --global user.name "Concourse CI"
@@ -14,7 +19,7 @@ if ! git diff --quiet HEAD --; then
   # check out branch because git-resource leaves us in `detached HEAD` state
   git checkout $DEPLOYMENTS_BRANCH
   git add .
-  git commit -m"Concourse CI automated $IAAS BOSH deployer :airplane:"
+  git commit -m"$GIT_COMMIT_MESSAGE"
 fi
 popd
 
