ada6e750cfdeea60c4cb052e2af5c346822ac5c7
Brian Cunnie <brian.cunnie@gmail.com>
2018-05-02 18:26:33 -0700

    Concourse has a vSphere worker, finally
    It has taken so long to deploy this that I've forgotten what I wanted it
    for.

---
+++ bin/concourse-worker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# usage:
+#   bin/concourse-worker.sh
+#
+# Deploys a Concourse Worker
+#
+set -eu
+
+DEPLOYMENTS_DIR="$( cd "${BASH_SOURCE[0]%/*}" && pwd )/.."
+
+pushd $DEPLOYMENTS_DIR/../concourse-bosh-deployment/cluster
+  git pull -r
+  bosh -e ipv4 deploy -d concourse-worker external-worker.yml \
+    -l ../versions.yml \
+    -v external_worker_network_name=guest \
+    -v worker_vm_type=concourse-workers \
+    -v instances=1 \
+    -v azs=[z1] \
+    -v deployment_name=concourse-worker \
+    -v tsa_host=ci.nono.io \
+    -v worker_tags=[vsphere] \
+    -l <(lpass show --note deployments.yml)
+popd
