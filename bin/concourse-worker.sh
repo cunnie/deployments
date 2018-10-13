901d79ce1b85aece0a1742c5776a5c7f4b2e4808
Brian Cunnie <brian.cunnie@gmail.com>
2018-10-13 15:14:03 -0700

    Cosmetic: Concourse worker deploy doesn't redact secrets

--- bin/concourse-worker.sh
+++ bin/concourse-worker.sh
@@ -11,7 +11,11 @@ DEPLOYMENTS_DIR="$( cd "${BASH_SOURCE[0]%/*}" && pwd )/.."
 
 pushd $DEPLOYMENTS_DIR/../concourse-bosh-deployment/cluster
   git pull -r
-  bosh -e ipv4 deploy -d concourse-worker external-worker.yml \
+  bosh \
+    -e ipv4 deploy \
+    -d concourse-worker \
+    external-worker.yml \
+    --no-redact \
     -l ../versions.yml \
     -v external_worker_network_name=guest \
     -v worker_vm_type=concourse-workers \
