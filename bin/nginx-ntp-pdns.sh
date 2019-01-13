96456c46bf2cbe5b6caa1c5f38e692327a4aaf4f
Brian Cunnie <brian.cunnie@gmail.com>
2019-01-13 06:31:46 -0800

    Templatize (sic) deployment of nginx + NTP + PowerDNS
    We simplify our deployment of nginx + NTP + PowerDNS because we'll be deploying
    two (AWS, Azure) of them, not one. Admittedly, for our present
    deployment of 1 server (AWS), this complicates things, for whereas before we
    had one manifest, now we have a script *and* a manifest.
    
    We extract the differing four variables to be easily set within our script:
    - IaaS (e.g. "aws")
    - Instance Type (e.g. "t3.nano")
    - External IP (e.g. "52.0.56.137")
    - sslip.io hostname (e.g. "52-0-56-137")
    
    It would be really nice to bring in our 3rd VM, the GCE VM, but that's a
    slightly different beast in that it also runs *Concourse* (the server), so its
    manifest is fairly different. We'll possibly address that at a later date.

---
+++ bin/nginx-ntp-pdns.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# usage:
+#   bin/nginx-ntp-pdns.sh
+#
+# Deploy nginx + NTP + PowerDNS servers to AWS, Azure, and GCE
+#
+DEPLOYMENTS_DIR="$( cd "${BASH_SOURCE[0]%/*}" && pwd )/.."
+
+if ! lpass status; then
+  echo "Log into LastPass!" >&2
+  exit 1
+fi
+
+set -- \
+  aws 52.0.56.137 52-0-56-137 t3.nano \
+
+while [ $# -gt 0 ]; do
+  IAAS=$1
+  bosh \
+    -e vsphere \
+    -d nginx-ntp-pdns-$IAAS \
+    deploy \
+    $DEPLOYMENTS_DIR/nginx-ntp-pdns.yml \
+    -l <(lpass show --note deployments.yml) \
+    -l <(curl -L https://raw.githubusercontent.com/cunnie/sslip.io/master/conf/sslip.io%2Bnono.io.yml) \
+    --no-redact \
+    \
+    -v iaas=$IAAS \
+    -v external_ip=$2 \
+    -v sslip_io_hostname=$3 \
+    -v vm_type=$4 \
+
+  shift 4
+done
