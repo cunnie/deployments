dabd2efb720f103f6880d211f18f472b06cbda32
Brian Cunnie <brian.cunnie@gmail.com>
2019-02-09 19:39:16 -0800

    Concourse worker is part of the mega-deployment
    - `bin/concourse-worker.sh` generates a manifest
      (for comparison)
    - that manifest is git-ignored.
    - remove the old, separate, Concourse GCE manifest

--- bin/concourse-worker.sh
+++ bin/concourse-worker.sh
@@ -12,10 +12,9 @@ DEPLOYMENTS_DIR="$( cd "${BASH_SOURCE[0]%/*}" && pwd )/.."
 pushd $DEPLOYMENTS_DIR/../concourse-bosh-deployment/cluster
   git pull -r
   bosh \
-    -e vsphere deploy \
+    int \
     -d concourse-worker \
     external-worker.yml \
-    --no-redact \
     -l ../versions.yml \
     -v external_worker_network_name=vsphere-guest \
     -v worker_vm_type=concourse-workers \
@@ -24,5 +23,5 @@ pushd $DEPLOYMENTS_DIR/../concourse-bosh-deployment/cluster
     -v deployment_name=concourse-worker \
     -v tsa_host=ci.nono.io \
     -v worker_tags=[] \
-    -l <(lpass show --note deployments.yml)
+    > $DEPLOYMENTS_DIR/concourse-worker.yml
 popd
