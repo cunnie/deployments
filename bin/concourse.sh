f872ab1912e6a4c539e2ee5c6f943ef6dfce2008
Brian Cunnie <brian.cunnie@gmail.com>
2019-06-12 19:19:53 -0700

    Concourse 5.3.0, uses Let's Encrypt

--- bin/concourse.sh
+++ bin/concourse.sh
@@ -24,10 +24,7 @@ bosh int $DEPLOYMENTS_DIR/../concourse-bosh-deployment/cluster/concourse.yml \
   -o $DEPLOYMENTS_DIR/../concourse-bosh-deployment/cluster/operations/static-web.yml \
   -o $DEPLOYMENTS_DIR/../concourse-bosh-deployment/cluster/operations/github-auth.yml \
   -o $DEPLOYMENTS_DIR/../concourse-bosh-deployment/cluster/operations/privileged-https.yml \
-  -o $DEPLOYMENTS_DIR/../concourse-bosh-deployment/cluster/operations/tls.yml \
-  \
-  --var-file atc_tls.certificate=etc/nono.io.crt \
-  --var      atc_tls.private_key="((nono_io_key))" \
+  -o $DEPLOYMENTS_DIR/../concourse-bosh-deployment/cluster/operations/enable-lets-encrypt.yml \
   \
   --var web_ip=104.155.144.4 \
   --var external_url=https://ci.nono.io \
