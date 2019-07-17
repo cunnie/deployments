c7b7ee1d637946aefa0e1b0b0854c382b00dd561
Brian Cunnie <brian.cunnie@gmail.com>
2019-07-16 18:19:55 -0700

    Assets needed to deploy Cloud Foundry
    We deploy Cloud Foundry with
    <https://github.com/cloudfoundry/cf-deployment> with several assets
    (scripts, manifest operations file, and var files)
    
    - We use HA Proxy; we don't have the luxury of a load balancer
    - We customize the CF cloud-config:
      - it's not the global cloud config; it's merely a "regular" config
      - there's overlap in the VM type "small"; we can only have one VM type
        "small", so I've made the global one match their specs & removed it
        from theirs
    - The BOSH DNS runtime config interferes with my DNS servers, so I've
      restricted it to the CF deployment.

---
+++ bin/cf.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# usage:
+#   bin/vsphere.sh
+#
+# Generate manifest for vSphere BOSH Director
+#
+# --var-errs: don't use; it flags the variables I'll interpolate the _next_ stage
+#
+# set up Let's Encrypt
+# export NSUPDATE_SERVER="ns-he.nono.io"
+# export NSUPDATE_KEY="$HOME/letsencrypt.key"
+# ~/.acme.sh/acme.sh --issue \
+#   -d *.cf.nono.io \
+#   --dns dns_nsupdate
+
+DEPLOYMENTS_DIR="$( cd "${BASH_SOURCE[0]%/*}" && pwd )/.."
+export BOSH_ENVIRONMENT=vsphere
+
+pushd $DEPLOYMENTS_DIR/../cf-deployment; git pull -r; popd
+
+# We don't use the primary config; instead we set a bunch of variables
+bosh update-config \
+  --non-interactive \
+  --type cloud \
+  --name cf \
+  <(bosh int \
+    -o $DEPLOYMENTS_DIR/cf/cloud-config-operations.yml \
+    -l $DEPLOYMENTS_DIR/cf/cloud-config-vars.yml \
+    $DEPLOYMENTS_DIR/../cf-deployment/iaas-support/vsphere/cloud-config.yml)
+
+bosh update-config \
+  --non-interactive \
+  --type runtime \
+  --name dns \
+  -o $DEPLOYMENTS_DIR/cf/dns-on-cf-only.yml \
+  $DEPLOYMENTS_DIR/../bosh-deployment/runtime-configs/dns.yml
+
+bosh \
+  -e vsphere \
+  -d cf \
+  deploy \
+  $DEPLOYMENTS_DIR/../cf-deployment/cf-deployment.yml \
+  -v system_domain=cf.nono.io \
+  -o $DEPLOYMENTS_DIR/../cf-deployment/operations/scale-to-one-az.yml \
+  -o $DEPLOYMENTS_DIR/../cf-deployment/operations/use-haproxy.yml \
+  -o $DEPLOYMENTS_DIR/../cf-deployment/operations/use-latest-stemcell.yml \
+  -v haproxy_private_ip=10.0.250.10 \
+
