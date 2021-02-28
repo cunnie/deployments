893cff4012a62f5874f110beb46a656642543321
Brian Cunnie <brian.cunnie@gmail.com>
2021-02-28 07:53:01 -0800

    🐞 Terraform: AWS: instance is ssh-able
    needed routing tables, internet gateway

--- terraform/aws/k8s.tf
+++ terraform/aws/k8s.tf
@@ -1,4 +1,11 @@
-# Thanks https://medium.com/@mattias.holmlund/setting-up-ipv6-on-amazon-with-terraform-e14b3bfef577
+# Create Infrastructure for sslip.io DNS server on AWS
+#
+# Use Graviton (ARM) instance type because I'm interested in a non-Intel
+# architecture.
+#
+# Thanks
+# https://medium.com/@mattias.holmlund/setting-up-ipv6-on-amazon-with-terraform-e14b3bfef577
+
 terraform {
   required_providers {
     aws = {
@@ -31,6 +38,9 @@ resource "aws_subnet" "k8s" {
   }
 }
 
+# No security groups-based firewall; the networking will be complicated enough
+# without worrying about security groups.
+
 resource "aws_security_group" "allow_everything" {
   name        = "allow_everything"
   description = "we are bold, we are brave, we are naked (as far as firewalls are concerned)"
@@ -56,11 +66,36 @@ resource "aws_security_group" "allow_everything" {
   }
 }
 
+resource "aws_internet_gateway" "k8s" {
+  vpc_id = aws_vpc.k8s.id
+}
+
+resource "aws_default_route_table" "k8s" {
+  default_route_table_id = aws_vpc.k8s.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.k8s.id
+  }
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.k8s.id
+  }
+}
+
+resource "aws_route_table_association" "k8s" {
+  subnet_id      = aws_subnet.k8s.id
+  route_table_id = aws_default_route_table.k8s.id
+}
+
 resource "aws_key_pair" "k8s" {
   key_name   = "k8s"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDksYxt4WhOyDZA/5jtY68Jp1NXDwj5yTiXvu5e0htyt1Z+oyp2exiNiiHcZ49e/DIb/+pLopxZJXndM+osYex7MuJ5Z2NLc9Dymj+zGKbfDatflcyNcqULA+Dtl4wrfEFWhZC0WoHY7f94MtszW0kU4jSuiP0IkQGw47XrsPe8irwQJK1O8mj5ygm6dsMcSRUV0fItltXGLQ95ANxg2YLOL9Kpbul2c6s08qcWJ35QBHTZyBP8Hryb2CkUdbW6sJCv5GuQ9DG0D2q5kYpSsznd3tnvl7AC3nUI4ENFaYF9LlYp28ohRG2LHl6A/r3u8ghYqSH5Qz4PV4CxX/Z0EEoSXflFWgYedb/5nYsEkly0DOpmOULqkobJg8ki3gBOzL5LEO8uI6uGzJz5XSbRzSDSTDPpUxcm5I5OA9kVTSq1jE7wxn4IA6kd1UvXRc2w/yxMa43W0L22lWHJjFIIbrYe6ymy6IxMmKLJpdh38aRXv7PzCM+rUmnm4Fpcko9xF68= brian.cunnie@gmail.com"
 }
 
+# remember, the user is "fedora" not "ec2-user"
+#
+# ssh -i ~/.ssh/aws fedora@w.x.y.z
+# curl https://raw.githubusercontent.com/cunnie/bin/master/install_fedora.sh | bash -x
 resource "aws_instance" "k8s" {
   ami                    = "ami-07b7fa952a4ad5fd2"
   key_name               = aws_key_pair.k8s.key_name
