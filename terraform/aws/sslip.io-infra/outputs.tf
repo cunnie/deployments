a8771d7b6a1be76aa3ecb2681923b3e07f20fefc
Brian Cunnie <brian.cunnie@gmail.com>
2021-06-10 07:26:07 -0700

    terraform AWS infra: README, shorter var outputs

--- terraform/aws/sslip.io-infra/outputs.tf
+++ terraform/aws/sslip.io-infra/outputs.tf
@@ -1,15 +1,15 @@
-output "aws_security_group_allow_everything_id" {
+output "security_group_sslip_io_id" {
   value = aws_security_group.allow_everything.id
 }
 
-output "aws_subnet_sslip_io_id" {
+output "subnet_sslip_io_id" {
   value = aws_subnet.sslip_io.id
 }
 
-output "aws_vpc_sslip_io_cidr_block" {
+output "vpc_sslip_io_cidr_block" {
   value = aws_vpc.sslip_io.cidr_block
 }
 
-output "aws_vpc_sslip_io_ipv6_cidr_block" {
+output "vpc_sslip_io_ipv6_cidr_block" {
   value = aws_vpc.sslip_io.ipv6_cidr_block
 }
