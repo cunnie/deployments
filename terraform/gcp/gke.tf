b0d8132bc005349d7c31b6894f7a2d93580c9355
Brian Cunnie <brian.cunnie@gmail.com>
2021-06-21 15:05:24 -0700

    🐞 Terraform GKE: No 404 on apply
    Container Node Pool must be in the same zone as the Container Cluster.
    
    fixes:
    ```
    │ Error: error creating NodePool: googleapi: Error 404: Not found: projects/blabbertabber/locations/us-central1/clusters/...
    ```

--- terraform/gcp/gke.tf
+++ terraform/gcp/gke.tf
@@ -31,7 +31,7 @@ resource "google_container_cluster" "primary" {
 # Separately Managed Node Pool
 resource "google_container_node_pool" "primary_nodes" {
   name       = "${google_container_cluster.primary.name}-node-pool"
-  location   = var.region
+  location   = "${var.region}-f"
   cluster    = google_container_cluster.primary.name
   node_count = var.gke_num_nodes
 
