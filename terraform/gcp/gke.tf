755d0cd97f93625092f2a1239bb9133f8906dd02
Brian Cunnie <brian.cunnie@gmail.com>
2021-06-21 13:21:20 -0700

    💵 Terraform GKE: 1 instance, not 3!
    By changing the location to a zone instead of a region, I cut down the
    number of instances from 3 to 1. The instances were in zones a, c, and
    f. I don't want to waste that much money!
    
    According to the
    [docs](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_cluster)
    
    > location - (Optional) The location (region or zone) in which the
    cluster master will be created, as well as the default node location. If
    you specify a zone (such as us-central1-a), the cluster will be a zonal
    cluster with a single cluster master. If you specify a region (such as
    us-west1), the cluster will be a regional cluster with multiple masters
    spread across zones in the region, and with default node locations in
    those zones as well

--- terraform/gcp/gke.tf
+++ terraform/gcp/gke.tf
@@ -16,7 +16,7 @@ variable "gke_num_nodes" {
 # GKE cluster
 resource "google_container_cluster" "primary" {
   name     = "${var.friendly_project_id}-gke"
-  location = var.region
+  location = "${var.region}-f"
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
