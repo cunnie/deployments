03c22e617bbe0c81d0d77cb716e4efae5e1a1ec0
Brian Cunnie <brian.cunnie@gmail.com>
2021-11-15 09:31:37 -0800

    GKE terraform: follow RAPID channel; region instead of zone
    I want to be on the latest release, so I'll follow the rapid channel
    from now on.
    
    And ever since I updated to 3 nodes, that means I can switch back to
    using a regional node cluster instead of a zonal node cluster.

--- terraform/gcp/gke/vpc.tf
+++ terraform/gcp/gke/vpc.tf
@@ -10,10 +10,6 @@ variable "region" {
   description = "region"
 }
 
-variable "zone" {
-  description = "zone within region"
-}
-
 provider "google" {
   project = var.project_id
   region  = var.region
