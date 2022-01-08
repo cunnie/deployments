8b063d9d8ae92f0e2f022ac5b7b4b9ba2cdf36fe
Brian Cunnie <brian.cunnie@gmail.com>
2022-01-08 09:42:09 -0800

    🐞 "Error: Output "zone" not found"
    I forgot to add the zone in the terraform outputs.

--- terraform/gcp/gke/outputs.tf
+++ terraform/gcp/gke/outputs.tf
@@ -3,6 +3,11 @@ output "region" {
   description = "GCloud Region"
 }
 
+output "zone" {
+  value       = var.zone
+  description = "GCloud Zone which has our cluster"
+}
+
 output "project_id" {
   value       = var.project_id
   description = "GCloud Project ID"
