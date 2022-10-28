181750825a17257163b022a27de60b0ebee98600
Brian Cunnie <brian.cunnie@gmail.com>
2022-10-28 16:40:50 -0700

    bosh-perf: various changes
    - Deploy 100 times instead of ten
    - stop loop if agent times-out
    - rename instance_group to something more generic, "stemcell"
      fewer changes when updating the stemcell.

--- bosh-perf/deploy.sh
+++ bosh-perf/deploy.sh
@@ -5,10 +5,17 @@ set -xue
 START=$(date +%s)
 SCRIPT_DIR=${BASH_SOURCE%/*}
 
-for i in $(seq 1 10); do
+for i in $(seq 1 100); do
     ITERATION_START=$(date +%s)
-    (export BOSH_DEPLOYMENT=dummy1; bosh deploy -n $SCRIPT_DIR/dummy1.yml; bosh -n deld) &
-    (export BOSH_DEPLOYMENT=dummy2; bosh deploy -n $SCRIPT_DIR/dummy2.yml; bosh -n deld) &
+    (export BOSH_DEPLOYMENT=dummy1; bosh deploy -n $SCRIPT_DIR/dummy1.yml) &
+    (export BOSH_DEPLOYMENT=dummy2; bosh deploy -n $SCRIPT_DIR/dummy2.yml) &
+    wait
+    if [ $(( $(date +%s) - ITERATION_START )) -gt "600" ]; then
+        echo "Exiting; live deployment with unresponsive VM"
+        exit 1
+    fi
+    (export BOSH_DEPLOYMENT=dummy1; bosh -n deld) &
+    (export BOSH_DEPLOYMENT=dummy2; bosh -n deld) &
     wait
     echo "Iteration $i elapsed seconds: $(( $(date +%s) - ITERATION_START ))"
 done
