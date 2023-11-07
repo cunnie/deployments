fd2adf679b1ee3815145082c67e26c43f31e3aab
Brian Cunnie <brian.cunnie@gmail.com>
2023-11-07 08:45:39 -0800

    Terraform GKE, again
    I had somehow wedged the state of my GKE Cluster (control plane couldn't
    reach the nodes) when I was trying to switch to a cheaper load balancer
    (standard tier not premium tier). I had to burn it all down & recreate
    it.

--- terraform/gcp/gke/gke.tf
+++ terraform/gcp/gke/gke.tf
@@ -9,7 +9,7 @@ variable "gke_password" {
 }
 
 variable "gke_num_nodes" {
-  default     = 3
+  default     = 4
   description = "number of gke nodes"
 }
 
@@ -23,6 +23,7 @@ resource "google_container_cluster" "primary" {
   # node pool and immediately delete it.
   remove_default_node_pool = true
   initial_node_count       = 1
+  deletion_protection = false
 
   network         = google_compute_network.vpc.name
   subnetwork      = google_compute_subnetwork.subnet.name
