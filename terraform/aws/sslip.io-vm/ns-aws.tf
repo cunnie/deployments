9759ca473315b11762c22e6b8296a8fafc74bd42
Brian Cunnie <brian.cunnie@gmail.com>
2024-06-27 15:23:52 -0700

    ns-aws: bump 22.04 (Jammy) → 24.04 (Noble)
    Now that AWS allows EC key pairs, let's switch to our standard key pair
    instead of our special-purpose RSA key pair.

--- terraform/aws/sslip.io-vm/ns-aws.tf
+++ terraform/aws/sslip.io-vm/ns-aws.tf
@@ -49,14 +49,14 @@ variable "vpc_sslip_io_ipv6_cidr_block" {
   description = "IPv6 CIDR"
 }
 
-resource "aws_key_pair" "sslip_io" {
-  key_name   = "sslip_io"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDksYxt4WhOyDZA/5jtY68Jp1NXDwj5yTiXvu5e0htyt1Z+oyp2exiNiiHcZ49e/DIb/+pLopxZJXndM+osYex7MuJ5Z2NLc9Dymj+zGKbfDatflcyNcqULA+Dtl4wrfEFWhZC0WoHY7f94MtszW0kU4jSuiP0IkQGw47XrsPe8irwQJK1O8mj5ygm6dsMcSRUV0fItltXGLQ95ANxg2YLOL9Kpbul2c6s08qcWJ35QBHTZyBP8Hryb2CkUdbW6sJCv5GuQ9DG0D2q5kYpSsznd3tnvl7AC3nUI4ENFaYF9LlYp28ohRG2LHl6A/r3u8ghYqSH5Qz4PV4CxX/Z0EEoSXflFWgYedb/5nYsEkly0DOpmOULqkobJg8ki3gBOzL5LEO8uI6uGzJz5XSbRzSDSTDPpUxcm5I5OA9kVTSq1jE7wxn4IA6kd1UvXRc2w/yxMa43W0L22lWHJjFIIbrYe6ymy6IxMmKLJpdh38aRXv7PzCM+rUmnm4Fpcko9xF68= brian.cunnie@gmail.com"
+resource "aws_key_pair" "nono" {
+  key_name   = "nono"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIWiAzxc4uovfaphO0QVC2w00YmzrogUpjAzvuqaQ9tD cunnie@nono.io"
 }
 
 resource "aws_instance" "sslip_io" {
-  ami           = "ami-065e36ba0c5d7ccc6" # "Ubuntu 22.04 - Jammy Jellyfish (arm64)"
-  key_name      = aws_key_pair.sslip_io.key_name
+  ami           = "ami-0eac975a54dfee8cb" # "Ubuntu Server 24.04 LTS (HVM), SSD Volume Type"
+  key_name      = aws_key_pair.nono.key_name
   instance_type = "t4g.micro"
   root_block_device {
     volume_size = 26 # the default 6 GB is too small; "/" is 86% full
